ea871116a7df78c0b8ba080605261850c6ac5864
MWaldrep2000 <matthew.a.waldrep@Knights.ucf.edu>
2021-03-17 22:05:14 -0400

    Added isSame() and isInside() methods to TimeBlock.java

--- Scheduler/TimeBlock.java
+++ Scheduler/TimeBlock.java
@@ -8,6 +8,8 @@
 	Time interval is fed in as a string with form <Xa-Yb> where X and Y are numbers
 	and a and b can either be 'a' or 'p' for AM and PM.
 
+	Also accepts ANY and NONE as arguments
+
 	X and Y are GUARANTEED to be 2 digits
 */
 
@@ -19,16 +21,20 @@ public class TimeBlock
 	// Boolean array of hours based on military time
 	// Length of 24
 	private boolean [] hours;
+
+	// Bounds inclusively between which the TimeBlock exists
+	private int [] bounds;
 	
 	// TimeBlock constructor taking a time block string as an argument
 	// init = the time interval as described in the description above
-	public TimeBlock(String init)
+	// throws a NumberFormatException if Integer.parseInt() fails for some reason
+	public TimeBlock(String init) throws NumberFormatException
 	{
 		// Initialize the hours array
-		hours = new boolean[NUM_HOURS];
+		this.hours = new boolean[NUM_HOURS];
 
 		// The boundaries on a time block
-		int [] bounds = new int[2];
+		this.bounds = new int[2];
 
 		// Set up the parse string, we will use it later
 		String [] parse;
@@ -36,8 +42,8 @@ public class TimeBlock
 		// If the init string is "ANY"
 		if (init.compareTo("ANY") == 0)
 		{
-			bounds[0] = 0;
-			bounds[1] = 23;
+			this.bounds[0] = 0;
+			this.bounds[1] = 23;
 
 			// parse will be set to an empty array
 			// will curcumvent the below for-loop
@@ -46,8 +52,8 @@ public class TimeBlock
 		// If the init string is "NONE"
 		else if (init.compareTo("NONE") == 0)
 		{
-			bounds[0] = -1;
-			bounds[1] = -1;
+			this.bounds[0] = -1;
+			this.bounds[1] = -1;
 
 			// parse will be set to an empty array
 			// will curcumvent the below for-loop
@@ -75,7 +81,7 @@ public class TimeBlock
 			hour = (parse[i].substring(2, 3).compareTo("p") == 0) ? hour + 12 : hour;
 
 			// Save the newly computed bound
-			bounds[i] = hour;
+			this.bounds[i] = hour;
 		}
 
 		// TODO: Implement checking for if the first boundary is larger than the second
@@ -83,7 +89,49 @@ public class TimeBlock
 		// Fill in the hours boolean array with the respective values
 		for (int i = 0; i < NUM_HOURS; i++)
 		{
-			hours[i] = (i >= bounds[0] && i <= bounds[1]);
+			hours[i] = (i >= this.bounds[0] && i <= this.bounds[1]);
 		}
+	}
+
+		// Public Methods //
+
+	// Gets the upper and lower boundaries of a TimeBlock
+	// Takes no arguments
+	// Returns a two-array containing the upper and lower bounds of the TimeBlock
+	public int [] getBounds()
+	{
+		// Return this object's bounds
+		return this.bounds;
+	}
+
+	// Checks if THIS TimeBlock is inside another TimeBlock
+	// tb = the TimeBlock to check if THIS TimeBlock is inside
+	// Returns a boolean to determine if THIS Timeblock is inside tb
+	public boolean isInside(TimeBlock tb)
+	{
+		// Get the bounds of tb	
+		int [] rBounds = tb.getBounds();
+
+		// Check if the bounds are the same
+		// If so, return true
+		if (this.isSame(tb))
+		{
+			return true;
+		}
+
+		// At this point, the two TimeBlocks aren't identical, return if lBounds is inside rBounds
+		return ((this.bounds[0] >= rBounds[0]) && (this.bounds[1] <= rBounds[1]));
+	}
+
+	// Checks if two TimeBlocks are the same
+	// tb = the TimeBlock to check if THIS TimeBlock is the same
+	// Returns a boolean to determine if THIS Timeblock has the same bounds as tb
+	public boolean isSame(TimeBlock tb)
+	{
+		// Get the bounds of tb	
+		int [] rBounds = tb.getBounds();
+		
+		// Logic to determine if both bounds are the same
+		return ((this.bounds[0] == rBounds[0]) && (this.bounds[1] == rBounds[1]));
 	}
 }
