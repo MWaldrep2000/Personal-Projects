ecbe7c5449996ffce8fb03368d5540f3cc643e6b
MWaldrep2000 <matthew.a.waldrep@gmail.com>
2022-04-06 21:21:00 -0400

    Added Card.py with partial description

---
+++ UNOSimulator/Card.py
@@ -0,0 +1,14 @@
+
+"""
+        Card.py
+
+    This file represents a singular card in an UNO deck. Each instantiation of this object
+    will consist of a color and value.
+
+    Allowable colors are RED, BLUE, GREEN, YELLOW, WILD, and WILD4+
+
+        - the first four colors are self-explanatory
+        - WILD pertains to a standard wild card
+        - WILD4+ pertains to a draw 4 wild card
+"""
+
